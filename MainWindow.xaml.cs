c49c64026338e68c5f4673afee0477e09d1d4ff9
booennm <booennm@gmail.com>
2025-01-11 12:46:24 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Xml.Linq;
+using WorkTimeTracker.ViewModels;
+
+namespace WorkTimeTracker
+{
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void HomePageViewControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            WorkTimeTracker.ViewModels.HomePageViewModel homePageVM = new();
+            HomePageViewControl.DataContext = homePageVM;
+        }
+    }
+}
---
+++ ViewModels/HomePageViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace WorkTimeTracker.ViewModels
+{
+    public class HomePageViewModel : INotifyPropertyChanged
+    {
+        private Stopwatch stopwatch = new();
+        private DispatcherTimer timer;
+
+        private string time;
+        public string Time
+        {
+            get { return time; }
+            set
+            {
+                time = value;
+                OnPropertyChanged(nameof(Time));
+            }
+        }
+
+        public ICommand StartTrackingCommand { get; }
+
+        public HomePageViewModel()
+        {
+            timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
+            timer.Tick += Timer_Tick;
+
+
+            StartTrackingCommand = new RelayCommand(_ => StartTracking());
+        }
+
+        private void StartTracking()
+        {
+            timer.Start();
+            stopwatch.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Time = stopwatch.Elapsed.ToString(@"hh\:mm\:ss");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
